d3301fb85de4871c40cb5882450bbdc848026109
carvajallaura041-byte <carvajallaura041@gmail.com>
2026-05-11 07:53:08 -0600

    Agregado backend con y conexión a MySQL para login

--- backend/app.js
+++ backend/app.js
@@ -1,38 +1,113 @@
 const express = require("express");
 const axios = require("axios");
+const cors = require("cors");
+const path = require("path");
+const mysql = require("mysql2");
 
 const app = express();
 
-app.get("/api/musica/:artista", async (req, res) => {
+app.use(cors());
+app.use(express.json());
 
-  try {
+app.use(express.static(path.join(__dirname, "../frontend")));
 
-    const artista = req.params.artista;
+const conexion = mysql.createConnection({
 
-    const respuesta = await axios.get(
-      `https://api.deezer.com/search?q=${artista}`
-    );
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "musicart"
 
-    const canciones = respuesta.data.data.map(cancion => ({
-      titulo: cancion.title,
-      artista: cancion.artist.name,
-      album: cancion.album.title,
-      imagen: cancion.album.cover,
-      preview: cancion.preview
-    }));
+});
 
-    res.json(canciones);
+conexion.connect(error => {
 
-  } catch (error) {
+    if(error){
 
-    res.status(500).json({
-      error: error.message
+        console.log("Error de conexión");
+
+    } else {
+
+        console.log("MySQL conectado");
+
+    }
+
+});
+
+app.post("/login", (req, res) => {
+
+    const usuario = req.body.usuario;
+    const password = req.body.password;
+
+    const sql =
+    "SELECT * FROM usuarios WHERE usuario = ? AND password = ?";
+
+    conexion.query(sql, [usuario, password], (error, resultado) => {
+
+        if(error){
+
+            return res.status(500).json({
+                acceso: false
+            });
+
+        }
+
+        if(resultado.length > 0){
+
+            res.json({
+                acceso: true
+            });
+
+        } else {
+
+            res.json({
+                acceso: false
+            });
+
+        }
+
     });
 
-  }
+});
+
+app.get("/api/musica/:artista", async (req, res) => {
+
+    try {
+
+        const artista = req.params.artista;
+
+        const respuesta = await axios.get(
+            `https://api.deezer.com/search?q=${artista}`
+        );
+
+        const canciones = respuesta.data.data.map(cancion => ({
+            titulo: cancion.title,
+            artista: cancion.artist.name,
+            album: cancion.album.title,
+            imagen: cancion.album.cover_big,
+            preview: cancion.preview
+        }));
+
+        res.json(canciones);
+
+    } catch (error) {
+
+        res.status(500).json({
+            error: "Error al obtener canciones"
+        });
+
+    }
+
+});
+
+app.get("/", (req, res) => {
+
+    res.sendFile(path.join(__dirname, "../frontend/login.html"));
 
 });
 
 app.listen(3000, () => {
-  console.log("Servidor corriendo en puerto 3000");
+
+    console.log("Servidor corriendo en http://localhost:3000");
+
 });
